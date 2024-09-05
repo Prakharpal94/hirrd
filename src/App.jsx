25219ee7ff771dec3f895aeec410ac739a0e5aae
Prakharpal94 <prakharpal94@gmail.com>
2024-09-05 16:31:49 +0530

    Deployment

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+
+
+import "./App.css";
+import AppLayout from "./layoutes/app-layout";
+import { ThemeProvider } from "./components/theme-provide";
+import ProtectedRoute from "./components/protected-route";
+import LandingPage from "./page/landing";
+import Onboarding from "./page/onboarding";
+import PostJob from "./page/post-job";
+import JobListing from "./page/job-listing";
+import MyJobs from "./page/my-jobs";
+import SavedJobs from "./page/saved-job";
+import JobPage from "./page/job";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
